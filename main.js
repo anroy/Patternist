c53611ff66ad9f55a34074f305d33280d44df57c
Arka Roy <arka.inago@gmail.com>
2020-05-04 00:34:01 +0900

    v0.0.1
    Can select rect or circle, specify size, and repeat in a 600x600 canvas.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function drawCanvas(width, height) {
 
 	// Get user parameters
+	var userShape = document.getElementById("UserShape").value;
+
+	var userWidth = document.getElementById("UserWidth").value;
+	var drawWidth = parseInt(userWidth);
+	var userHeight = document.getElementById("UserHeight").value;
+	var drawHeight = parseInt(userHeight);
+
 	var userColor = document.getElementById("UserColor").value;
 
 	// Initialize canvas variables
@@ -9,5 +16,41 @@ function drawCanvas(width, height) {
 
 	// Draw to context
 	ctx.fillStyle = userColor;
-	ctx.fillRect(10, 10, 200, 200);
+
+	var horizCnt = 600 / (drawWidth+10) - 1;
+	var vertCnt = 600 / (drawHeight+10) - 1;
+	var i, j;
+	var xPos, yPos;
+
+	if( userShape == "circle") {
+		var radius = drawWidth;
+		var diameter = radius * 2;
+
+		//ctx.beginPath();
+		//ctx.arc(50, 50, 80, 0, 2 * Math.PI);
+		//ctx.fill();
+		//ctx.stroke();
+
+		for (j = 0; j < vertCnt; j++) {
+			for (i = 0; i < horizCnt; i++) {
+				xPos = 10 + ((diameter+10) * i);
+				yPos = 10 + ((diameter+10) * j);
+				ctx.beginPath();
+				ctx.arc(xPos, yPos, radius, 0, 2 * Math.PI);
+				ctx.fill();
+			}
+		}
+
+	}
+	else if( userShape == "rectangle") {
+
+		for (j = 0; j < vertCnt; j++) {
+			for (i = 0; i < horizCnt; i++) {
+				xPos = 10 + ((drawWidth+10) * i);
+				yPos = 10 + ((drawHeight+10) * j);
+				ctx.fillRect(xPos, yPos, drawWidth, drawHeight);
+			}
+		}
+	}
+
 }
