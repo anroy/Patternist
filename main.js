02787e3b864e1ae0f169a6800beb52cd4643842c
Arka Roy <arka@arkaroy.com>
2020-05-05 00:26:45 +0900

    v0.0.2 Select image and make patter.

--- main.js
+++ main.js
@@ -1,56 +1,91 @@
-function drawCanvas(width, height) {
+function drawCanvas() {
+	// Initialize canvas variables
+	const canvas = document.getElementById("MainCanvas");
+	const ctx = canvas.getContext("2d");
+
+	var userShape;
+	var userWidth, width;
+	var userHeight, height;
 
 	// Get user parameters
-	var userShape = document.getElementById("UserShape").value;
+	userShape = document.getElementById("UserShape").value;
+	userWidth = document.getElementById("UserWidth").value;
+	width = parseInt(userWidth);
+	ctx.fillStyle = document.getElementById("UserColor").value;
 
-	var userWidth = document.getElementById("UserWidth").value;
-	var drawWidth = parseInt(userWidth);
-	var userHeight = document.getElementById("UserHeight").value;
-	var drawHeight = parseInt(userHeight);
+	switch(userShape)
+	{
+		case "rectangle":
+			userHeight = document.getElementById("UserHeight").value;
+			height = parseInt(userHeight);
+			drawRectangle(canvas, ctx, width, height);
+			break;
 
-	var userColor = document.getElementById("UserColor").value;
+		case "circle":
+			drawCircle(canvas, ctx, width);
+			break;
+	
+		default:
+			break;
+	}
+}
 
-	// Initialize canvas variables
-	var canvas = document.getElementById("MainCanvas");
-	var ctx = canvas.getContext("2d");
-
-	// Draw to context
-	ctx.fillStyle = userColor;
-
-	var horizCnt = 600 / (drawWidth+10) - 1;
-	var vertCnt = 600 / (drawHeight+10) - 1;
+function drawRectangle(canvas, ctx, width, height) {
+	var horizCnt = canvas.width / (width+10);
+	var vertCnt = canvas.height / (height+10);
 	var i, j;
 	var xPos, yPos;
 
-	if( userShape == "circle") {
-		var radius = drawWidth;
-		var diameter = radius * 2;
-
-		//ctx.beginPath();
-		//ctx.arc(50, 50, 80, 0, 2 * Math.PI);
-		//ctx.fill();
-		//ctx.stroke();
-
-		for (j = 0; j < vertCnt; j++) {
-			for (i = 0; i < horizCnt; i++) {
-				xPos = 10 + ((diameter+10) * i);
-				yPos = 10 + ((diameter+10) * j);
-				ctx.beginPath();
-				ctx.arc(xPos, yPos, radius, 0, 2 * Math.PI);
-				ctx.fill();
-			}
-		}
-
-	}
-	else if( userShape == "rectangle") {
-
-		for (j = 0; j < vertCnt; j++) {
-			for (i = 0; i < horizCnt; i++) {
-				xPos = 10 + ((drawWidth+10) * i);
-				yPos = 10 + ((drawHeight+10) * j);
-				ctx.fillRect(xPos, yPos, drawWidth, drawHeight);
-			}
+	for (j = 0; j < vertCnt; j++) {
+		for (i = 0; i < horizCnt; i++) {
+			xPos = (width+10) * i;
+			yPos = (height+10) * j;
+			ctx.fillRect(xPos, yPos, width, height);
 		}
 	}
+}
 
+function drawCircle(canvas, ctx, radius) {
+	var diameter = radius * 2;
+	var horizCnt = canvas.width / (diameter+10);
+	var vertCnt = canvas.height / (diameter+10);
+	var i, j;
+	var xPos, yPos;
+
+	for (j = 0; j < vertCnt; j++) {
+		for (i = 0; i < horizCnt; i++) {
+			xPos = radius + (diameter+10) * i;
+			yPos = radius + (diameter+10) * j;
+			ctx.beginPath();
+			ctx.arc(xPos, yPos, radius, 0, 2 * Math.PI);
+			ctx.fill();
+		}
+	}
+}
+
+function drawImage(canvas, ctx, img) {
+	//var img = document.getElementById("LocalFile");
+	//alert(img.width);
+	//alert(img.height);
+	var horizCnt = canvas.width / img.width;
+	var vertCnt = canvas.height / img.height;
+	var i, j;
+	var xPos, yPos;
+
+	for (j = 0; j < vertCnt; j++) {
+		for (i = 0; i < horizCnt; i++) {
+			xPos = img.width * i;
+			yPos = img.height * j;
+			ctx.drawImage(img, xPos, yPos);
+		}
+	}
+}
+
+function clearCanvas() {
+	// Initialize canvas variables
+	const canvas = document.getElementById("MainCanvas");
+	const ctx = canvas.getContext("2d");
+
+	// clear
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
