55fd708ac3765d7b3fe490d62bd119951219f122
Arka Roy <arka@arkaroy.com>
2020-05-06 21:00:39 +0900

    v0.0.4
    Started rotation of images.
    Drawing stroke triangles.
    Annotating symbols with loop index.

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 function drawCanvas() {
+	//alert("Enter drawCanvas");
+
 	// Initialize canvas variables
 	const canvas = el("MainCanvas");
 	const ctx = canvas.getContext("2d");
 
 	var userShape;
-	var userWidth, width;
-	var userHeight, height;
+	var userSize;
 
 	// Get user parameters
 	userShape = el("UserShape").value;
-	userWidth = el("UserWidth").value;
-	width = parseInt(userWidth);
+	userSize = parseInt(el("UserSize").value);
 	ctx.fillStyle = el("UserColor").value;
+	ctx.strokeStyle = el("UserColor").value;
 
 	switch(userShape)
 	{
-		case "rectangle":
-			userHeight = el("UserHeight").value;
-			height = parseInt(userHeight);
-			drawRectangle(canvas, ctx, width, height);
+		case "square":
+			drawSquare(canvas, ctx, userSize);
 			break;
 
 		case "circle":
-			drawCircle(canvas, ctx, width);
+			drawCircle(canvas, ctx, userSize);
+			break;
+
+		case "triangle":
+			drawTriangle(canvas, ctx, userSize);
 			break;
 	
 		default:
@@ -30,17 +33,45 @@ function drawCanvas() {
 	}
 }
 
-function drawRectangle(canvas, ctx, width, height) {
-	var horizCnt = canvas.width / (width+10);
-	var vertCnt = canvas.height / (height+10);
+function drawSquare(canvas, ctx, size) {
+	//alert("Enter drawSquare");
+
+	var horizCnt = canvas.width / (size+10);
+	var vertCnt = canvas.height / (size+10);
 	var i, j;
 	var xPos, yPos;
 
 	for (j = 0; j < vertCnt; j++) {
 		for (i = 0; i < horizCnt; i++) {
-			xPos = (width+10) * i;
-			yPos = (height+10) * j;
-			ctx.fillRect(xPos, yPos, width, height);
+			xPos = (size+10) * i;
+			yPos = (size+10) * j;
+			ctx.fillRect(xPos, yPos, size, size);
+		}
+	}
+}
+
+function drawTriangle(canvas, ctx, size) {
+	//alert("Enter drawTriangle");
+
+	var horizCnt = canvas.width / size;
+	var vertCnt = canvas.height / size;
+	var i, j;
+	var xPos, yPos;
+	var coordText;
+
+	for (j = 0; j < vertCnt; j++) {
+		for (i = 0; i < horizCnt; i++) {
+			xPos = size * i;
+			yPos = size * j;
+			ctx.beginPath();
+			ctx.moveTo(xPos + (size * 0.5), yPos);	// Start at middle top
+			ctx.lineTo(xPos, yPos + size);			// Draw to left bottom
+			ctx.lineTo(xPos + size, yPos + size);	// Draw to right bottom
+			ctx.lineTo(xPos + (size * 0.5), yPos);	// Draw back to middle top
+			ctx.closePath();
+			ctx.stroke();
+			coordText = j.toString() + "," + i.toString();
+			ctx.fillText(coordText, xPos + (size * 0.4), yPos + (size * 0.7));
 		}
 	}
 }
@@ -86,12 +117,23 @@ function drawImage(canvas, ctx, img) {
 	var vertCnt = canvas.height / img.height;
 	var i, j;
 	var xPos, yPos;
+	var coordText;
 
 	for (j = 0; j < vertCnt; j++) {
 		for (i = 0; i < horizCnt; i++) {
 			xPos = img.width * i;
 			yPos = img.height * j;
+			ctx.save();
+			//ctx.setTransform(1,0,0,1,0,0);
+			ctx.translate( xPos + (0.5 * img.width), yPos + (0.5 * img.height));
+			ctx.rotate(Math.PI/2);
 			ctx.drawImage(img, xPos, yPos);
+
+			ctx.strokeStyle = "red";
+			coordText = j.toString() + "," + i.toString();
+			ctx.fillText(coordText, xPos + (img.width * 0.4), yPos + (img.height * 0.7));
+
+			ctx.restore();
 		}
 	}
 }
